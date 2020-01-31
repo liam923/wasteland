0b9070020c1ebac7732d41b3007ed7dfd4b9c898
Liam Stevenson <liam923@verizon.net>
2020-01-30 19:43:48 -0500

    Configure iOS project

---
+++ Wasteland-iOS/Wasteland/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wasteland
+//
+//  Created by Liam Stevenson on 1/30/20.
+//  Copyright © 2020 Liam Stevenson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
