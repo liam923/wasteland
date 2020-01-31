f7b878daaf5c7e39ce3addd0917132c60c793c27
Liam Stevenson <liam923@verizon.net>
2020-01-30 20:06:57 -0500

    Configure Xcode project. Create .gitignore.

---
+++ Wasteland-iOS/Pods/Target Support Files/Pods-Wasteland/Pods-Wasteland-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wasteland : NSObject
+@end
+@implementation PodsDummy_Pods_Wasteland
+@end
---
+++ Wasteland-iOS/Pods/Target Support Files/Pods-WastelandUITests/Pods-WastelandUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WastelandUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WastelandUITests
+@end
