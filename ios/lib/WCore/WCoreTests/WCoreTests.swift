cc7c5f9254d591078754c04cad64d8ad7dca480e
Liam Stevenson <liam923@verizon.net>
2020-04-27 21:22:53 -0400

    fix: Test core module
    Make tests for core module work.

--- ios/lib/WCore/WCoreTests/WCoreTests.swift
+++ ios/lib/WCore/WCoreTests/WCoreTests.swift
@@ -21,14 +21,5 @@ class WCoreTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(false)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
